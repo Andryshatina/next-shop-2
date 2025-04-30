5cd2f359e5985b9cb571fe22b9a324810a7ade1a
Andrushatina <droid.rttd@gmail.com>
2025-04-30 16:02:43 +0300

    type fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,9 +2,9 @@ import { Product } from "@/types/product";
 import { getProductById } from "@/lib/api";
 import ProductDetails from "@/components/ProductDetails";
 
-const ProductPage = async ({ params }: { params: { id: number } }) => {
+const ProductPage = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
-  const product: Product = await getProductById(id);
+  const product: Product = await getProductById(Number(id));
 
   return <ProductDetails product={product} />;
 };
