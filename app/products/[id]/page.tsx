4979d187abb39267e70fb2594f3eae0fb0e4da2d
Andrushatina <droid.rttd@gmail.com>
2025-04-30 16:37:31 +0300

    final props fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,12 +2,9 @@ import { Product } from "@/types/product";
 import { getProductById } from "@/lib/api";
 import ProductDetails from "@/components/ProductDetails";
 
-type PageProps = {
-  params: { id: string };
-};
-
-const ProductPage = async ({ params }: PageProps) => {
-  const { id } = await params;
+type paramsType = Promise<{ id: string }>;
+const ProductPage = async (props: { params: paramsType }) => {
+  const { id } = await props.params;
   const product: Product = await getProductById(Number(id));
 
   return <ProductDetails product={product} />;
